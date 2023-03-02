d747654ebb509c96b2605a81e7aeeee390a5d740
Luvai Hassanali <luvaihassanali@gmail.com>
2023-03-02 08:37:56 -0500

    update ps-scripts

--- post-build.py
+++ post-build.py
@@ -11,6 +11,16 @@ file.write_text(file.read_text().replace('<source src=', '<source data-src='))
 file.write_text(file.read_text().replace('lazy" src', 'lazy" data-src'))
 file.write_text(file.read_text().replace('<video class', '<video playsinline="" class'))
 file.write_text(file.read_text().replace('script.min.js', 'index.js'))
+file.write_text(file.read_text().replace('''</script>
+</head>''', '</script></head>'))
 
 file = Path('cv/cv.html')
 file.write_text(file.read_text().replace('<script src="../assets/js/script.min.js"></script>', ''))
+file.write_text(file.read_text().replace('''</script>
+</head>''', '</script></head>'))
+
+
+file = Path('cv/cv1.html')
+file.write_text(file.read_text().replace('<script src="../assets/js/script.min.js"></script>', ''))
+file.write_text(file.read_text().replace('''</script>
+</head>''', '</script></head>'))
